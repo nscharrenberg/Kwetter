63c5c1cac43f91366e69bb4a7309d3959de76a94
Noah Scharrenberg <nscharrenberg@hotmail.com>
2019-02-21 10:18:34 +0100

    LikeTweet test added

---
+++ src/test/java/kwetter/features/LikeTweet.feature
@@ -0,0 +1,36 @@
+Feature: User should be able to like a tweet
+  As a user I want to like a tweet so that I can interact with another user.
+
+  Scenario: Like a Tweet
+    Given the following role:
+      | member |
+    Given the following users:
+      | username          | testUser              | testUser2            |
+      | biography         | This is my biography  | This is my biography |
+      | locationLongitude | 123123.123123         | 12345456.234234      |
+      | locationLatitude  | 123653.234123         | 89237489.23423       |
+      | website           | wwww.mysite.io        | www.mysite.com       |
+      | role              |                       |                      |
+    Given the following tweet:
+      | message           | This is a tweet       |
+      | author            | testUser2             |
+      | createdAt         | 12/01/2019            |
+    When testUser likes a Tweet
+    Then the Tweet should have one like by testUser
+
+  Scenario: Unlike a Tweet
+    Given the following role:
+      | member |
+    Given the following users:
+      | username          | testUser              | testUser2            |
+      | biography         | This is my biography  | This is my biography |
+      | locationLongitude | 123123.123123         | 12345456.234234      |
+      | locationLatitude  | 123653.234123         | 89237489.23423       |
+      | website           | wwww.mysite.io        | www.mysite.com       |
+      | role              |                       |                      |
+    Given the following tweet:
+      | message           | This is a tweet       |
+      | author            | testUser2             |
+      | createdAt         | 12/01/2019            |
+    When testUser unlikes a Tweet
+    Then the Tweet should not have a like by testUser anymore
---
+++ src/test/java/kwetter/steps/LikeTweetStepDef.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2019. Noah Scharrenberg
+ */
+
+package kwetter.steps;
+
+import cucumber.api.DataTable;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.*;
+import kwetter.container.WorldContainer;
+import model.Tweet;
+
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class LikeTweetStepDef {
+    private WorldContainer worldContainer;
+    private Tweet tweet;
+
+    public LikeTweetStepDef(WorldContainer worldContainer) {
+        this.worldContainer = worldContainer;
+    }
+
+    @Given("^the following tweet:$")
+    public void the_following_tweet(DataTable arg1) throws Exception {
+        List<Tweet> tweets = arg1.transpose().asList(Tweet.class);
+        tweet = tweets.get(0);
+        tweet.setAuthor(this.worldContainer.users.get(1));
+    }
+
+    @When("^testUser likes a Tweet$")
+    public void testuser_likes_a_Tweet() throws Exception {
+        tweet.addLike(this.worldContainer.users.get(0));
+    }
+
+    @Then("^the Tweet should have one like by testUser$")
+    public void the_Tweet_should_have_one_like_by_testUser() throws Exception {
+        assertFalse(tweet.getLikes().isEmpty());
+    }
+
+    @When("^testUser unlikes a Tweet$")
+    public void testuser_unlikes_a_Tweet() throws Exception {
+        tweet.removeLike(this.worldContainer.users.get(0));
+    }
+
+    @Then("^the Tweet should not have a like by testUser anymore$")
+    public void the_Tweet_should_not_have_a_like_by_testUser_anymore() throws Exception {
+        assertTrue(tweet.getLikes().isEmpty());
+    }
+
+    @Then("^an Exception StringToLongException saying \"([^\"]*)\" is thrown\\.$")
+    public void an_Exception_StringToLongException_saying_is_thrown(String arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
