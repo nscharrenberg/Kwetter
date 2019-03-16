63cbf4d0fadb41273770f2f7014e45d6ac5c2873
Noah Scharrenberg <nscharrenberg@hotmail.com>
2019-03-16 16:41:27 +0100

    jax-analyzer added for swagger docs + password removed from dto

--- src/main/java/dtos/users/UserCleanDto.java
+++ src/main/java/dtos/users/UserCleanDto.java
@@ -4,8 +4,8 @@ import dtos.roles.RoleCleanDto;
 
 public class UserCleanDto {
     private Integer id;
+    private String username;
     private String email;
-    private String password;
     private String biography;
     private String website;
     private Double longitude;
@@ -20,20 +20,20 @@ public class UserCleanDto {
         this.id = id;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getBiography() {
